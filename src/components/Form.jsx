e6144c8ebba85e0aedb45ea5a1a2a5ad82d098c9
s0d4z3r0 <av.falcao@hotmail.com>
2023-06-27 18:25:47 -0300

    Adicionado validações e captura dos states

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,61 @@
+import { useState } from "react";
 import "./Form.css";
 
+
 const Form = () => {
+    const [age, setAge] = useState('')
+    const [gender, setGender] = useState('')
+    const [weight, setWeight] = useState('')
+    const [height, setHeight] = useState('')
+    const [lifestyle, setLifestyle] = useState('')
+    const [objective, setObjective] = useState('')
+
+    const [TMB, setTMB] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(age <= 0 || age === ''){
+            alert('Digite sua Idade.')
+            return
+        }else if(gender === ''){
+            alert('Selecione o Sexo.')
+            return
+        }else if(weight <= 0 || weight === '' || weight.toString().length > 5 ){
+            alert('Digite um Peso (em kg) válido.')
+            return
+        }else if(height <= 0 || height === '' || height.toString().length > 3 ){
+            alert('Digite uma Altura (em cm) válida.')
+            return
+        }else if(lifestyle === ''){
+            alert('Selecione o Nível de Atividade.')
+            return
+        }else if(objective === ''){
+            alert('Selecione seu Objetivo.')
+            return
+        }
+
+
+        console.log(age, gender, weight, height, lifestyle, objective)
+        setAge('')
+        setGender('')
+        setWeight('')
+        setHeight('')
+        setLifestyle('')
+        setObjective('')
+    }
+
+    
   return (
     <div className="form">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Idade:</span>
-          <input type="text" />
+          <input type="number" placeholder="Idade em anos" onChange={e => setAge(e.target.value)} value={age}/>
         </label>
         <label>
           <span>Sexo:</span>
-          <select>
+          <select onChange={e => setGender(e.target.value)} value={gender}>
             <option value="" ></option>
             <option value="M">Masculino</option>
             <option value="F">Feminino</option>
@@ -18,15 +63,15 @@ const Form = () => {
         </label>
         <label>
           <span>Peso:</span>
-          <input type="text" placeholder="Peso em kg (Ex: 65.5)"/>
+          <input type="number" placeholder="Peso em kg (Ex: 65.5)" onChange={e => setWeight(e.target.value)} value={weight} maxLength="4"/>
         </label>
         <label>
           <span>Altura:</span>
-          <input type="text" placeholder="Altura em cm (ex: 170)"/>
+          <input type="number" placeholder="Altura em cm (ex: 170)" onChange={e => setHeight(e.target.value)} value={height}/>
         </label>
         <label>
           <span>Nível de Atividade:</span>
-          <select name="" id="">
+          <select onChange={e => setLifestyle(e.target.value)} value={lifestyle}>
             <option value="" ></option>
             <option value="1.2">
               Sedentário (sem exercício/esporte algum)
@@ -48,14 +93,14 @@ const Form = () => {
         </label>
         <label>
           <span>Objetivo:</span>
-          <select>
+          <select onChange={e => setObjective(e.target.value)} value={objective}>
             <option value="" ></option>
             <option value="500">Emagrecer</option>
             <option value="0">Manter</option>
             <option value="501">Ganhar peso</option>
           </select>
         </label>
-        <button>CALCULAR</button>
+        <button type="submit">CALCULAR</button>
       </form>
     </div>
   );
